120c0c58d8641a5e8b17ec2d6243fb235b2f6670
DESKTOP-FUED73H\Gary <gary.newelluk@gmail.com>
2022-06-10 20:27:21 +0100

    fixed links

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,13 +1,15 @@
 import { FaEdit, FaPlus, FaTimes, FaSortUp, FaSortDown, FaSave, FaWindowClose, FaBookOpen } from 'react-icons/fa'
 import PersonalDetailsSample  from './images/PersonalDetailsSample.PNG'
 import PersonalDetailsSampleEdit  from './images/PersonalDetailsSampleEdit.PNG'
+import { Link } from 'react-router-dom';
+
 const StartPage = () => {
 
     return (
         <div className="StartPage">
             <h1 className="MainHeader">Free Online CV Generator</h1>            
             <div id="Main">
-                <a href="/cvcreator/CreateCV">
+                <Link to='/cvcreator/CreateCV'>
                 <div className="box">
                     <h1>Create A CV From Scratch</h1>
                     <p>
@@ -17,8 +19,8 @@ const StartPage = () => {
                         All the entries are blank, leaving a clean canvas to write the CV as you see fit.
                     </p>
                 </div>
-                </a>
-                <a href="/cvcreator/SampleResumeGenerator">
+                </Link>
+                <Link to='/cvcreator/SampleResumeGenerator'>
                 <div className="box">
                     <h1>Create A CV Using Sample Data</h1>
                     <p>
@@ -28,33 +30,33 @@ const StartPage = () => {
                         Sample data has been added to give you hints on how to fill in the various sections.
                     </p>
                 </div>
-                </a>
-                <a href="/cvcreator/LoadCV">
+                </Link>
+                <Link to='/cvcreator/LoadCV'>
                 <div className="box">
                     <h1>Load A CV</h1>
                     <p>
                         Click here to load a CV created previously using this tool.
                     </p>
                 </div>
-                </a>
+                </Link>
             </div>
             <div id="Main">
-                <a href="/cvcreator/UserGuide">
+                <Link to='/cvcreator/UserGuide'>
                 <div className="box">
                     <h1>User Guide</h1>
                     <p>
                         Click here for a user guide to help you fill in your CV using the CV Creator.
                     </p>
                 </div>
-                </a>
-                <a href="/About">
+                </Link>
+                <Link to='/cvcreator/About'>                
                 <div className="box">
                     <h1>About Free Online CV Generator</h1>
                     <p>
                         Click here for more information about this tool and how your personal information isn't used.
                     </p>
                 </div>
-                </a>
+                </Link>
                 <div className="box">
                 </div>
             </div>            
